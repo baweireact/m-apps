b00e13d565abfb5a097c3ddc81f0ed79bc825b22
xutongbao <1183391880@qq.com>
2019-12-09 11:31:36 +0800

    1

---
+++ m-app-1707B/m-game/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    questionList: [],
+    currentIndex: 0,
+    name: ''
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ m-app-1707B/m-game/vue.config.js
@@ -0,0 +1,54 @@
+
+let questionList = [
+  {
+    id: 0,
+    question: '你会选择如何度过一个有趣的周末？',
+    yourAnswer: '',
+    answerList: [{
+      id: 0,
+      text: '写代码',
+      value: 'A'
+    }, {
+      id: 1,
+      text: '逛街',
+      value: 'B'
+    }, {
+      id: 2,
+      text: '看电影',
+      value: 'C'
+    }]
+  },
+  {
+    id: 1,
+    question: '你爱吃什么？',
+    yourAnswer: '',
+    answerList: [{
+      id: 0,
+      text: '火锅',
+      value: 'A'
+    }, {
+      id: 1,
+      text: '肯德基',
+      value: 'B'
+    }, {
+      id: 2,
+      text: '麻辣烫',
+      value: 'C'
+    }]
+  },
+]
+
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    before(app) {
+      app.get('/api/question_list', (req, res) => {
+        res.send({
+          code: 200,
+          data: questionList,
+          message: '游戏人格测试题'
+        })
+      })
+    }
+  }
+}
