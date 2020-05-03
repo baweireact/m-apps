1b3ce531a31bf30cfa12b0a65faf14afa3f200b8
xutongbao <1183391880@qq.com>
2020-05-03 09:22:45 +0800

    1

--- demo/ts/m-app/index.ts
+++ demo/ts/m-app/index.ts
@@ -1,9 +1,27 @@
-let a = 'hello!2'
-
-function greeter(name: string) {
-  return  `hello world!${name}`
+//泛型类
+class ArrayList<T> {
+  array: T[] = []
+  size: number
+  add(item: T) {
+    this.array.push(item)
+    this.size = this.array.length
+  }
+  //共用一套逻辑
+  print() {
+    console.log(`全部数据:${this.array.join(' ')}`)
+  }
 }
 
-document.body.innerHTML = greeter('xu2')
+//创建对象时才明确类型
+let numberList = new ArrayList<number>()
+numberList.add(1) //如果是错误类型，会报错
+numberList.add(2)
+console.log(numberList.size)
+numberList.print()
 
-
+let strList = new ArrayList<string>()
+strList.add('xu')
+strList.add('徐')
+strList.add('hello')
+console.log(strList.size)
+strList.print()
