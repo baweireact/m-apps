de4d7c2169df0418f9fdcf7cd35aa4d4a3efcab3
xutongbao <1183391880@qq.com>
2021-10-27 19:10:36 +0800

    优化

--- demo/nextjs/nextjs-blog/pages/light/home/Home.js
+++ demo/nextjs/nextjs-blog/pages/light/home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 
-export default function Home({ list }) {
+export default function Home({ serverSideList }) {
   return (
     <div className="m-wrap">
       <Head>
@@ -10,30 +10,50 @@ export default function Home({ list }) {
       </Head>
       hello
       <div>
-        {
-          list.map(item => (
-            <div key={item.id}>{item.title}</div>
-          ))
-        }
+        {/* {list.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))} */}
+        {serverSideList.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
       </div>
     </div>
   )
 }
 
-export async function getStaticProps() {
-  const list = [
+// export async function getStaticProps() {
+//   const list = [
+//     {
+//       id: 0,
+//       title: 'xu1',
+//     },
+//     {
+//       id: 1,
+//       title: 'admin',
+//     },
+//   ]
+//   return {
+//     props: {
+//       list,
+//     },
+//   }
+// }
+
+export async function getServerSideProps(context) {
+  console.log(context.query)
+  const serverSideList = [
     {
       id: 0,
-      title: 'xu',
+      title: 'xu_s',
     },
     {
       id: 1,
-      title: 'admin',
+      title: 'admin_s',
     },
   ]
   return {
     props: {
-      list,
+      serverSideList,
     },
   }
 }
---
+++ demo/nextjs/nextjs-blog/pages/light/home/[id].js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function blog({ params }) {
+  return <div>{params.id}</div>
+}
+
+export async function getStaticProps({ params }) {
+  console.log(params)
+  return {
+    props: {
+      params
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = [
+    {
+      params: {
+        id: '1',
+      },
+    },
+    {
+      params: {
+        id: '2',
+      },
+    },
+  ]
+  return {
+    paths,
+    fallback: false,
+  }
+}
