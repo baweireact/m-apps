877984856a85930b80edda15e70c403f11ba85ac
xutongbao <1183391880@qq.com>
2021-10-28 15:38:19 +0800

    优化

--- demo/nextjs/nextjs-blog/ecosystem.config.js
+++ demo/nextjs/nextjs-blog/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'my-nuxtjs-app',
+      name: `${process.env.name}`,
       exec_mode: 'cluster',
       instances: 2, //工作线程数量,如果给定的数字为0，PM2则会根据你CPU核心的数量来生成对应的工作线程。
       script: './node_modules/next/dist/bin/next',
