2d0038f993ae6e1734407f7e276cb8ca7318c9a4
xutongbao@douyuxingchen.com <xutongbao@douyuxingchen.com>
2022-04-15 19:00:27 +0800

    1

--- demo/vue/m-app5/src/api/config.js
+++ demo/vue/m-app5/src/api/config.js
@@ -2,8 +2,9 @@
 const baseURL = {
   dev: 'http://localhost:81',
   test: 'http://10.3.138.173:81',
+  pre: 'http://10.3.138.173:81',
   prod: 'http://39.97.238.175:81'
-}['dev']
+}[process.env.VUE_APP_NODE_ENV]
 
 console.log(666, process.env)
 
--- demo/vue/m-app5/vue.config.js
+++ demo/vue/m-app5/vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: (config) => {
+    console.log('old webpackConfig', config)
+    config.output.chunkFilename = 'js/[name]-legacy-[hash].js'
+    if (process.env.VUE_APP_NODE_ENV === 'dev') {
+      config.devtool = 'eval-source-map'
+    } else {
+      config.devtool = 'source-map'
+    }
+    console.log('new webpackConfig', config)
+  },
 })
