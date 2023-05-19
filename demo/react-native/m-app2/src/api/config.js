5f37057118d6c5af16a44f7f0c81e247ed8ea36e
xutongbao@douyuxingchen.com <xutongbao@douyuxingchen.com>
2023-05-19 13:38:05 +0800

    1

--- demo/react-native/m-app2/src/api/config.js
+++ demo/react-native/m-app2/src/api/config.js
@@ -1,11 +1,11 @@
 import Constants from 'expo-constants'
 
 const baseURL = {
-  dev: 'http://10.3.152.15:81',
-  test: 'http://10.3.152.15:81',
+  dev: 'http://172.20.10.4:81',
+  test: 'http://172.20.10.4:81',
   prod: 'http://39.97.238.175:81'
 }[Constants.manifest.extra.REACT_APP_MODE]
 
-console.log(666, process.env)
+console.log(666, process.env, baseURL)
 
 export { baseURL }
